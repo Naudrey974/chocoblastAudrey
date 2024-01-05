60aa9ae366f49838f8aae6236fefd6717950c722
audrey <audrey.simon@live.fr>
2024-01-05 16:22:29 +0100

    choco utilisateur corrigé

--- app/vue/vue_add_utilisateur.php
+++ app/vue/vue_add_utilisateur.php
@@ -2,28 +2,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter un utilisateur</title>
+    <title>S'inscrire</title>
 </head>
 <body>
-    <?php include_once './app/vue/vue_navbar.php'?>
-    <h1>Ajouter un utilisateur :</h1>
-    <form action="" method="post">
-        <label for="nom_utilisateur">Saisir le nom de l'utilisateur :</label>
+    <?php include_once './app/vue/vue_navbar.php';?>
+    <h1>S'inscrire :</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="nom_utilisateur">Saisir le nom :</label>
         <input type="text" name="nom_utilisateur">
-        <label for="prenom_utilisateur">Saisir le prénom de l'utilisateur :</label>
+        <label for="prenom_utilisateur">Saisir le prénom :</label>
         <input type="text" name="prenom_utilisateur">
-        <label for="mail_utilisateur">Saisir l'email de l'utilisateur :</label>
+        <label for="mail_utilisateur">Saisir le mail :</label>
         <input type="email" name="mail_utilisateur">
-        <label for="password_utilisateur">Saisir le MP de l'utilisateur :</label>
+        <label for="password_utilisateur">Saisir le mot de passe :</label>
         <input type="password" name="password_utilisateur">
-        <label for="password_vérification">Confirmer le MP de l'utilisateur :</label>
-        <input type="password" name="password_vérification">
-        <label for="image_utilisateur">Enregistrer une image :</label>
-        <input type="file" id="image" alt="Login" src="" />
-        <label for="id_roles">Saisir le N° de rôles de l'utilisateur :</label>
-        <input type="text" name="id_roles">
-        <input type="submit" value="Ajouter" name="submit">
+        <label for="password_confirmation">Confirmer le mot de passe :</label>
+        <input type="password" name="password_confirmation">
+        <input type="file" name="image_utilisateur">
+        <input type="submit" value="S'inscire" name="submit">
     </form>
-    <p><?=$message?></p>
+    <p><?= $message ?></p>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,11 +5,11 @@ include_once './app/utils/bddConnect.php';
 include_once './app/utils/utilitaire.php';
 //importer les model (requête SQL)
 include_once './app/model/model_roles.php';
-include_once './app/model/model_utilisateurs.php';
+include_once './app/model/model_utilisateur.php';
 //imports des controllers
 include_once './app/controller/ctrl_page.php';
 include_once './app/controller/ctrl_roles.php';
-include_once './app/controller/ctrl_utilisateurs.php';
+include_once './app/controller/ctrl_utilisateur.php';
 //utilisation de session_start(pour gérer la connexion au serveur)
 session_start();
 //Analyse de l'URL avec parse_url() et retourne ses composants
@@ -24,7 +24,7 @@ switch ($path) {
         exemple();
         break;
     case '/mvccyber/roles/add':
-        insertRoles($bdd);
+        addRoles($bdd);
         break;
     case '/mvccyber/roles/all':
         showAllRoles($bdd);
@@ -34,7 +34,7 @@ switch ($path) {
         break;
     case '/mvccyber/utilisateur/add':
         addUtilisateur($bdd);
-        break;    
+        break;
     default:
         error();
         break;
